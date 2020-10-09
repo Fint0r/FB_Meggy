698400b0363899ad483b2a6fb98edf8a631023b9
Fint0r <fintor976@gmail.com>
2020-10-09 20:45:17 +0200

    fix

--- fb.py
+++ fb.py
@@ -1,10 +1,11 @@
 from fbchat import log, Client
 from fbchat.models import *
-import getpass
+
 
 class EchoBot(Client):
     meggy_counter = 0
     jatekosok = []
+    code = ''
 
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
         self.markAsDelivered(thread_id, message_object.uid)
@@ -15,6 +16,10 @@ class EchoBot(Client):
         if author_id != self.uid:
             sender_name = self.fetchUserInfo(author_id)[author_id].name
 
+            if 'kód:' in message_object.text or 'Kód:' in message_object.text or 'kod:' in message_object.text or 'Kod:' in message_object.text:
+                self.code = message_object.text.split(':')[1].strip()
+                message_object.text = f'Kód {self.code} mentve.'
+                self.send(message_object, thread_id=thread_id, thread_type=thread_type)
             if message_object.text == '-':
                 if sender_name in self.jatekosok:
                     self.jatekosok.remove(sender_name)
@@ -41,23 +46,24 @@ class EchoBot(Client):
                 self.send(message_object, thread_id=thread_id, thread_type=thread_type)
 
             if message_object.text == 'salty' or message_object.text == 'Salty' or message_object.text == 'fintor' or message_object.text == 'Fintor':
-                self.sendLocalImage('..\pics\salty.png', thread_id=thread_id, thread_type=thread_type)
+                self.sendLocalImage('salty.png', thread_id=thread_id, thread_type=thread_type)
 
             if message_object.text == 'Timo vacsora!' or message_object.text == 'timo vacsora!' or message_object.text == 'Timo vacsora' or message_object.text == 'timo vacsora':
-                self.sendLocalImage('..\pics\timo_vacsora.png', thread_id=thread_id, thread_type=thread_type)
+                self.sendLocalImage('timo_vacsora.png', thread_id=thread_id, thread_type=thread_type)
 
             if message_object.text == '€':
                 message_object.text = 'Fintor gyere vedelni!'
                 self.send(message_object, thread_id=thread_id, thread_type=thread_type)
 
-            if message_object.text == 'reset' or message_object.text == 'Reset':
+            if message_object.text == 'reset' or message_object.text == 'Reset' or message_object.text == 'Clear' or message_object.text == 'clear':
                 self.meggy_counter = 0
                 self.jatekosok = []
+                self.code = ''
                 message_object.text = 'Reseteltem a countert!'
                 self.send(message_object, thread_id=thread_id, thread_type=thread_type)
 
             if message_object.text == 'help' or message_object.text == 'Help':
-                message_object.text = 'Dobd a 🍒-et, ha játszanál!\nReset-eld a countert a reset-tel!'
+                message_object.text = '🍒 - Ha játszanál\nReset, Clear - Ha vége a játéknak\nKód: ABCDEQ - Ha mentenéd a kódot\n- - Ha mégsem jössz\nEmergency meeting, EM - Ha kevesen vagyunk\n€ - Ha inni kell\nTimo vacsora! - Ha Timo éhes\nSalty - Ha hülye vagy'
                 self.send(message_object, thread_id=thread_id, thread_type=thread_type)
 
             if message_object.text == '🍒':
@@ -65,7 +71,10 @@ class EchoBot(Client):
                     self.jatekosok.append(sender_name)
                     self.meggy_counter += 1
                     names = '\t\n'.join(self.jatekosok)
-                    message_object.text = f'Játszani akarók száma: {str(self.meggy_counter)}\n{names}'
+                    if self.code != '':
+                        message_object.text = f'Játszani akarók száma: {str(self.meggy_counter)}\nJátékkód: {self.code}\n{names}'
+                    else:
+                        message_object.text = f'Játszani akarók száma: {str(self.meggy_counter)}\n{names}'
                     self.send(message_object, thread_id=thread_id, thread_type=thread_type)
                     if self.meggy_counter > 5:
                         message_object.text = 'Meg is vagyunk! Indulhat a játék!'
@@ -75,5 +84,5 @@ class EchoBot(Client):
                     self.send(message_object, thread_id=thread_id, thread_type=thread_type)
 
 
-client = EchoBot('your mail', getpass.getpass())
+client = EchoBot('', '')
 client.listen()
